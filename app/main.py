2ae19062ec7c3247bce3bacf8da2ce7ea7653fd4
Adenupe <adenupekuye@gmail.com>
2026-05-25 13:59:07 -0500

    updated the main.py file with a new route and changed Mongo to lowercase

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
+import os
 from fastapi import FastAPI
 from app.routes import tasks
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+app_name = os.getenv("APP_NAME")
+mongo_url = os.getenv("MONGO_URL")
+print(mongo_url)
 
 app = FastAPI()
 
 app.include_router(tasks.router)
+
+@app.get("/")
+def home():
+    return {"status":"app is live"}
+
+@app.get("/health")
+def health():
+    return {"status":"Healthy"}
+
+@app.get("/info")
+def info():
+    return {"name of application is: ": app_name}
